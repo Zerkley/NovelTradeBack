c014acac2aca3f59bf55bc8ad3d97bedf82d0420
zerkley <miguelmr94@gmail.com>
2024-04-13 18:18:08 +0100

    added swagger documentation

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var createError = require('http-errors');
 var express = require('express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 var path = require('path');
+const m2s = require('mongoose-to-swagger');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require("dotenv").config();
@@ -9,6 +12,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 const { default: mongoose } = require('mongoose');
+const UserModel = require('./models/users_model');
 
 var app = express();
 
@@ -24,6 +28,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+const UserDefinition = m2s(UserModel);
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My API',
+      version: '1.0.0',
+      description: 'A simple Express API',
+    },
+    components:{
+      schemas:{
+        User: UserDefinition
+      }
+    }
+  },
+  apis: ['./routes/*.js'], // path to your route files
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
 // catch 404 and forward to error handler
