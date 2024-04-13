756f424c7b22521f21074cd7c879e0ee36909411
zerkley <miguelmr94@gmail.com>
2024-04-13 10:59:37 +0100

    first connection with DB

---
+++ app.js
@@ -0,0 +1,39 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+require("dotenv").config();
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const { default: mongoose } = require('mongoose');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+main().catch(err => console.error(err));
+
+async function main(){
+  await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@prod.u537eha.mongodb.net/?retryWrites=true&w=majority&appName=Prod`)
+}
+
+module.exports = app;
